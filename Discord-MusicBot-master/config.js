977e29c876ad25dde52d16dc0f1056cc22b19ac1
MagmaMango <86885815+MagmaMango@users.noreply.github.com>
2021-08-14 18:42:12 +0530

    Update config.js

--- Discord-MusicBot-master/config.js
+++ Discord-MusicBot-master/config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
+  Admins: [""], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
+  SupportServer: "will tell later", //Support Server Link
+  Token: process.env.Token || "ODc2MDg4MDA3MDc2MjM3MzEy.YRe-iQ.BcKEcJWoY5k61AkC6_1HxzlTOzI", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Magma_Mango", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
@@ -19,9 +19,9 @@ module.exports = {
   //Lavalink
    Lavalink: {
     id: "Main",
-    host: "lava.link",
-    port: 80,
-    pass: "youshallnotpass", 
+    host: "lava.sudhan.tech",
+    port: 1234,
+    pass: "CodingWithSudhan", 
   },
   
   //Alternate Lavalink
@@ -36,7 +36,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "29a5d46459154a30be8e636c64fa425a", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "4c9ce98fdc88480f9b4e396b2efca5fd", //Spotify Client Secret
   },
 };
